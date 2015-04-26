0c1c5c86e73996d7a4fb6769b60df95cc9050a77
Sanjib Ahmad <sanjib@sanjib.org>
2015-04-26 21:13:07 +0600

    Create Meme model

---
+++ MemeMe/Models/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Sanjib Ahmad on 4/26/15.
+//  Copyright (c) 2015 Object Coder. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String!
+    var bottomText: String!
+    var originalImage: UIImage!
+    var memeImage: UIImage!
+}
